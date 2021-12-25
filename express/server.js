cc3afd49943309bd0890a331fee853b154928757
Andrey Stalker <shtieynikov@gmail.com>
2021-12-25 16:58:04 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -7,12 +7,15 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-    if (req.query.url) {res.redirect(req.query.url)} else {res.status(400).send("no url provided")}
+    //if (req.query.url) {res.redirect(req.query.url)} else {res.status(400).send("no url provided")}
+    var idAddress = request.connection.remoteAddress;
+    res.send(200, "ti lox, " + idAddress)
+    res.end()
 });
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../redirect/index.html')));
+//app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../redirect/index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
